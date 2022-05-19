4fdbf0a3e41929ebff2fc480d5f9e35b6d6cf015
Cyriaque <cyriaquedelaunay@gmail.com>
2022-05-19 20:27:30 +0200

    enable scan in spanish for mand mx

--- test/app.test.ts
+++ test/app.test.ts
@@ -21,6 +21,15 @@ describe('Scrap data', () => {
     expect(storeData.options.data.$PBSB.name).to.equal('Noir Uni');
   });
 
+  it('should scrap data for a model3 in Spanish', async () => {
+    const defaultStoreData = getDefaultStoreData('model3');
+    const rawData = await getRawStoreData('es-ES', 'model3');
+    expect(rawData).to.be.an('object');
+    const storeData = hydrateStoreData(rawData, defaultStoreData);
+    expect(storeData.options.data.$PBSB).to.be.an('object');
+    expect(storeData.options.data.$PBSB.name).to.equal('Negro sólido');
+  });
+
   it('should scrap data for a modelx in English', async () => {
     const defaultStoreData = getDefaultStoreData('modelx');
     const rawData = await getRawStoreData('en-US', 'modelx');
