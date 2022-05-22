f03ddab2dc785a4eba114c6cda6b08485dcac89c
Cyriaque <cyriaquedelaunay@gmail.com>
2022-05-22 20:31:58 +0200

    updated tests

--- test/app.test.ts
+++ test/app.test.ts
@@ -3,18 +3,18 @@ import { expect } from 'chai';
 import { getDefaultStoreData, getRawStoreData, hydrateStoreData } from '../utils';
 
 describe('Scrap data', () => {
-  it('should scrap data for a model3 in English', async () => {
-    const defaultStoreData = getDefaultStoreData('model3');
-    const rawData = await getRawStoreData('en-US', 'model3');
+  it('should scrap data for a model3 in English (US)', async () => {
+    const defaultStoreData = getDefaultStoreData('m3');
+    const rawData = await getRawStoreData('en-US', 'm3');
     expect(rawData).to.be.an('object');
     const storeData = hydrateStoreData(rawData, defaultStoreData);
     expect(storeData.options.data.$PBSB).to.be.an('object');
     expect(storeData.options.data.$PBSB.name).to.equal('Solid Black');
   });
 
-  it('should scrap data for a model3 in English', async () => {
-    const defaultStoreData = getDefaultStoreData('model3');
-    const rawData = await getRawStoreData('fr-FR', 'model3');
+  it('should scrap data for a model3 in French', async () => {
+    const defaultStoreData = getDefaultStoreData('m3');
+    const rawData = await getRawStoreData('fr-FR', 'm3');
     expect(rawData).to.be.an('object');
     const storeData = hydrateStoreData(rawData, defaultStoreData);
     expect(storeData.options.data.$PBSB).to.be.an('object');
@@ -22,27 +22,27 @@ describe('Scrap data', () => {
   });
 
   it('should scrap data for a model3 in Spanish', async () => {
-    const defaultStoreData = getDefaultStoreData('model3');
-    const rawData = await getRawStoreData('es-ES', 'model3');
+    const defaultStoreData = getDefaultStoreData('m3');
+    const rawData = await getRawStoreData('es-ES', 'm3');
     expect(rawData).to.be.an('object');
     const storeData = hydrateStoreData(rawData, defaultStoreData);
     expect(storeData.options.data.$PBSB).to.be.an('object');
     expect(storeData.options.data.$PBSB.name).to.equal('Negro sólido');
   });
 
-  it('should scrap data for a modelx in English', async () => {
-    const defaultStoreData = getDefaultStoreData('modelx');
-    const rawData = await getRawStoreData('en-US', 'modelx');
+  it('should scrap data for a modelx in English (US)', async () => {
+    const defaultStoreData = getDefaultStoreData('mx');
+    const rawData = await getRawStoreData('en-US', 'mx');
     expect(rawData).to.be.an('object');
     const storeData = hydrateStoreData(rawData, defaultStoreData);
     expect(storeData.options.data.$PBSB).to.be.an('object');
   });
 
-  // it('should scrap data for a modely in English', async () => {
-  //   const defaultStoreData = getDefaultStoreData('modelx');
-  //   const rawData = await getRawStoreData('en-US', 'modely');
-  //   expect(rawData).to.be.an('object');
-  //   const storeData = hydrateStoreData(rawData, defaultStoreData);
-  //   expect(storeData.options.data.$PBSB).to.be.an('object');
-  // });
+  it('should scrap data for a modely in English (US)', async () => {
+    const defaultStoreData = getDefaultStoreData('my');
+    const rawData = await getRawStoreData('en-US', 'my');
+    expect(rawData).to.be.an('object');
+    const storeData = hydrateStoreData(rawData, defaultStoreData);
+    expect(storeData.options.data.$PBSB).to.be.an('object');
+  });
 });
